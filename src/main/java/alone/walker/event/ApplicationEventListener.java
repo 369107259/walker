8996ec1fefe3e84a948fe8015e18f8c26f93a696
huangyong <huangyong>
2020-07-02 15:11:57 +0800

    commit

--- src/main/java/alone/walker/event/ApplicationEventListener.java
+++ src/main/java/alone/walker/event/ApplicationEventListener.java
@@ -14,7 +14,6 @@ import java.util.Map;
 public class ApplicationEventListener {
     public void doSomething(){
         // TODO: 2020/7/1   处理相应的事情
-        int i = 1/0;
         System.out.println("接受事件处理完成");
     }
 
